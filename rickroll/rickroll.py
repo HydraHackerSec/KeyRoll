0bb9076f400b30c15f73d25e3d00918d606ce21a
HydraHackerSec <157287105+HydraHackerSec@users.noreply.github.com>
2024-02-02 12:09:07 +0100

    Update rickroll.py

--- rickroll/rickroll.py
+++ rickroll/rickroll.py
@@ -1,3 +1,21 @@
+'''
+KeyRoll is a keylogger developed in Python as part of a school project for the Red Teaming course at our Cyber Security school.
+Copyright (C) 2024  Federico Carioni - Vincenzo Torchia
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
 import tkinter as tk
 import multiprocessing
 import platform
